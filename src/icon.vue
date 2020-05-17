d719be8e3ebbf9d019bd682d1bcc90eb0b493d1f
hex <982492477@qq.com>
2020-05-17 12:58:34 +0800

    icon组件抽离

---
+++ src/icon.vue
@@ -0,0 +1,18 @@
+<template>
+  <svg class="h-icon">
+    <use :xlink:href="`#i${name}`"></use>
+  </svg>
+</template>
+
+<script>
+export default {
+  props: ["name"],
+}
+</script>
+
+<style lang="scss">
+.h-icon {
+  width: 1em;
+  height: 1em;
+}
+</style>
