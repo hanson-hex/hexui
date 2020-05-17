d14a5071d6c3439703adcd0b30cce81c1d1ef900
hex <982492477@qq.com>
2020-05-18 00:11:09 +0800

    使用chai.expect添加测试用例

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
-import Vue from "vue"
+import vue from "vue"
 import Button from "./button"
 import Icon from "./icon"
 import ButtonGroup from "./button-group"
 
-Vue.component("h-button", Button)
-Vue.component("h-icon", Icon)
-Vue.component("h-button-group", ButtonGroup)
+vue.component("h-button", Button)
+vue.component("h-icon", Icon)
+vue.component("h-button-group", ButtonGroup)
 
-new Vue({
+new vue({
   el: "#app",
   data: {
     loading1: false,
@@ -15,3 +15,90 @@ new Vue({
     loading3: false,
   },
 })
+
+import chai from "chai"
+
+const expect = chai.expect
+// 单元测试 动态生成按钮
+{
+  const constructor = vue.extend(Button)
+  const button = new constructor({
+    propsData: {
+      icon: "setting",
+    },
+  })
+  button.$mount()
+  let useElement = button.$el.querySelector("use")
+  let href = useElement.getAttribute("xlink:href")
+  expect(href).to.eq("#isetting")
+  button.$el.remove()
+  button.$destroy()
+}
+{
+  const constructor = vue.extend(Button)
+  const button = new constructor({
+    propsData: {
+      icon: "setting",
+      loading: true,
+    },
+  })
+  button.$mount()
+  let useElement = button.$el.querySelector("use")
+  let href = useElement.getAttribute("xlink:href")
+  expect(href).to.eq("#iloading")
+  button.$el.remove()
+  button.$destroy()
+}
+
+{
+  const div = document.createElement("div")
+  document.body.appendChild(div)
+  const constructor = vue.extend(Button)
+  const button = new constructor({
+    propsData: {
+      icon: "setting",
+      loading: true,
+    },
+  })
+  button.$mount(div)
+  let svg = button.$el.querySelector("svg")
+  let { order } = window.getComputedStyle(svg)
+  expect(order).to.eq("1")
+  button.$el.remove()
+  button.$destroy()
+}
+
+{
+  const div = document.createElement("div")
+  document.body.appendChild(div)
+  const constructor = vue.extend(Button)
+  const button = new constructor({
+    propsData: {
+      icon: "setting",
+      loading: true,
+      iconPosition: "right",
+    },
+  })
+  button.$mount(div)
+  let svg = button.$el.querySelector("svg")
+  let { order } = window.getComputedStyle(svg)
+  expect(order).to.eq("2")
+  button.$el.remove()
+  button.$destroy()
+}
+{
+  const constructor = vue.extend(Button)
+  const hButton = new constructor({
+    propsData: {
+      icon: "setting",
+      loading: true,
+      iconposition: "right",
+    },
+  })
+  hButton.$mount()
+  hButton.$on("click", function() {
+    expect(1).to.eq(1)
+  })
+  let button = hButton.$el
+  button.click()
+}
