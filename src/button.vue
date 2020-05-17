322fa45a1e6ae9d9f94366ce3cfee1d4cd33ea5c
hex <982492477@qq.com>
2020-05-17 12:47:53 +0800

    新增icon及对应props

--- src/button.vue
+++ src/button.vue
@@ -1,20 +1,33 @@
 <template>
-  <button class="h-button">按钮</button>
+  <button class="h-button" :class="{ [`icon-${iconPosition}`]: true }">
+    <svg v-if="icon" class="icon">
+      <use :xlink:href="`#i${icon}`"></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 
 <script>
-export default {}
+export default {
+  props: ["icon", "iconPosition"],
+}
 </script>
 
 <style lang="scss">
 .h-button {
   font-size: var(--font-size);
   height: var(--button-height);
-  padding: 0 1em; /*width*/
+  padding: 0 0.8em; /*width*/
   font: inherit;
+  vertical-align: middle;
   border: 1px solid var(--border-color);
   border-radius: var(--border-radius);
   background: var(--button-bg);
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
   &:hover {
     border-color: var(--border-color-hover);
   }
@@ -23,6 +36,23 @@ export default {}
   }
   &:focus {
     outline: none;
+  }
+  > .icon {
+    order: 1;
+    margin-right: 0.3em;
+  }
+  > .content {
+    order: 2;
+  }
+}
+.icon-right {
+  > .icon {
+    order: 2;
+    margin-left: 0.3em;
+    margin-right: 0;
+  }
+  > .content {
+    order: 1;
   }
 }
 </style>
