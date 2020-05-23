7ee16e09f975fa28da8bc46da46845e71b481ca6
hex <982492477@qq.com>
2020-05-23 08:51:56 +0800

    完成row col基础布局

--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@ import Button from "./button"
 import Icon from "./icon"
 import ButtonGroup from "./button-group"
 import Input from "./input"
+import Col from "./col"
+import Row from "./row"
 
 Vue.component("h-button", Button)
 Vue.component("h-icon", Icon)
 Vue.component("h-button-group", ButtonGroup)
 Vue.component("h-input", Input)
+Vue.component("h-row", Row)
+Vue.component("h-col", Col)
 
 new Vue({
   el: "#app",
---
+++ src/col.vue
@@ -0,0 +1,31 @@
+.<template>
+  <div class="col" :class="[`col-${span}`]">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HCol",
+  props: {
+    span: {
+      type: [Number, String],
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.col {
+  width: 50%;
+  height: 100px;
+  background: gray;
+  border: 1px solid red;
+  $class: col-;
+  @for $n from 1 through 24 {
+    &.#{$class}#{$n} {
+      width: ($n/24) * 100%;
+    }
+  }
+}
+</style>
