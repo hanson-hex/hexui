a3f75b1d2c5ebe0757c1f75db9fbb251b0d8f6cb
hex <982492477@qq.com>
2020-05-23 18:34:43 +0800

    完成基本布局动画

--- src/sider.vue
+++ src/sider.vue
@@ -1,13 +1,38 @@
 <template>
-  <div class="sider">
-    <slot></slot>
-  </div>
+  <transition name="fade">
+    <div class="sider" v-show="visible">
+      <slot></slot>
+      <button @click="visible = !visible">关闭</button>
+    </div>
+  </transition>
 </template>
 
 <script>
 export default {
   name: "HSider",
+  data() {
+    return {
+      visible: true,
+    }
+  },
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.sider {
+  position: relative;
+  > button {
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.5s;
+}
+.fade-enter,
+.fade-leave-to {
+  margin-left: -200px;
+}
+</style>
