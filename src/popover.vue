24e49cfb8382026da44f3320d74d153eb8b7252a
hex <982492477@qq.com>
2020-06-03 23:20:13 +0800

    popover移到最外层

--- src/popover.vue
+++ src/popover.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="popover" @click.stop="xxx()">
-    <div class="content-wrapper" v-if="visible" @click.stop>
+    <div
+      class="content-wrapper"
+      ref="contentWrapper"
+      v-show="visible"
+      @click="zzz"
+    >
       <slot name="content"></slot>
     </div>
-    <slot name="trigger"></slot>
+    <span ref="triggerWrapper">
+      <slot name="trigger"></slot>
+    </span>
   </div>
 </template>
 
@@ -16,19 +23,33 @@ export default {
     }
   },
   methods: {
+    zzz() {
+      console.log("zzz")
+    },
     xxx() {
       this.visible = !this.visible
-      console.log("设置了监听")
-      this.$nextTick(() => {
-        console.log("开启监听")
-        let eventHander = () => {
-          this.visible = false
-          document.removeEventListener("click", eventHander)
-        }
-        document.addEventListener("click", eventHander)
-      })
+      if (this.visible) {
+        this.$nextTick(() => {
+          document.body.appendChild(this.$refs.contentWrapper)
+          let {
+            width,
+            height,
+            left,
+            top,
+          } = this.$refs.triggerWrapper.getBoundingClientRect()
+          console.log("wdith", width)
+          this.$refs.contentWrapper.style.left = `${left}px`
+          this.$refs.contentWrapper.style.top = `${top}px`
+          let eventHander = () => {
+            this.visible = false
+            document.removeEventListener("click", eventHander)
+          }
+          document.addEventListener("click", eventHander)
+        })
+      }
     },
   },
+  mounted() {},
 }
 </script>
 
@@ -36,12 +57,11 @@ export default {
 .popover {
   display: inline-block;
   position: relative;
-  .content-wrapper {
-    position: absolute;
-    left: 0;
-    bottom: 100%;
-    border: 1px solid red;
-    box-shadow: 0 0 3px rgba(0, 0, 0.5);
-  }
+}
+.content-wrapper {
+  position: absolute;
+  transform: translateY(-100%);
+  border: 1px solid red;
+  box-shadow: 0 0 3px rgba(0, 0, 0.5);
 }
 </style>
